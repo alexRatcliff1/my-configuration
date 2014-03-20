833355a53cce34f279b18f701da368f960ad5e16
Alex Ratcliff <alexratcliff1@gmail.com>
2014-03-20 17:52:36 -0500

    Add system non-specific bash configuration to repo
    Source the file in .bashrc

---
+++ bash-config.sh
@@ -0,0 +1,8 @@
+#Add my-config/bin to the path
+export PATH=$PATH:$(dirname "${BASH_SOURCE[0]}")/bin
+
+#add some aliases that I like
+alias sl='ls'
+alias greprn='grep -rn'
+alias greprin='grep -rin'
+
